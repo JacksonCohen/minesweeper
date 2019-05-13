892d3b3be754b61c5e1d06388a4cafcd620b3700
Jackson <jacksonrcohen@gmail.com>
2019-05-13 17:07:26 -0400

    Set up basic client and server

---
+++ client/src/components/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component, Fragment } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div>Rendering!</div>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
